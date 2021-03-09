977c4d733b1025d49f9f386237a31c1c30df7ef2
aadilow <abeiladilow@gmail.comgit config user.email abeiladilow@gmail.com>
2021-03-09 17:56:49 -0500

    Update

---
+++ Quiz_1/QuizStudy/src/quiz/quiztest.java
@@ -0,0 +1,18 @@
+package quiz;
+
+public class quiztest {
+
+	public static void main(String[] args) {
+		char a=1;
+		int b='a';
+		char c=97;
+		char d='a';
+		char e=97+1;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+	}
+
+}
